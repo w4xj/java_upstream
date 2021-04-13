2a7c753b3efcdf56b095c9844d7ddcc80ef3651f
lemon joker <novicedriver@163.com>
2021-04-14 00:12:08 +0800

    feat: 1.线程池相关

---
+++ java_02_advanced/src/main/java/indi/w4xj/juc/threadpool/code/ParallelStreamTest.java
@@ -0,0 +1,42 @@
+package indi.w4xj.juc.threadpool.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @Author lemon joker
+ * @Project java_upstream
+ * @Package indi.w4xj.juc.threadpool.code
+ * @Classname ParalleStreamTest
+ * @Description TODO
+ * @Date 2021/4/13 23:34
+ * @Created by IntelliJ IDEA
+ */
+public class ParallelStreamTest {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        Random r = new Random();
+        for(int i=0; i<10000; i++) nums.add(1000000 + r.nextInt(1000000));
+
+        //System.out.println(nums);
+
+        long start = System.currentTimeMillis();
+        nums.forEach(v->isPrime(v));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+        start = System.currentTimeMillis();
+        nums.parallelStream().forEach(ParallelStreamTest::isPrime);
+        end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+    }
+
+    static boolean isPrime(int num) {
+        for(int i=2; i<=num/2; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ java_02_advanced/src/main/java/indi/w4xj/juc/threadpool/code/WorkStealingPoolTest.java
@@ -0,0 +1,54 @@
+package indi.w4xj.juc.threadpool.code;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author lemon joker
+ * @Project java_upstream
+ * @Package indi.w4xj.juc.threadpool.code
+ * @Classname WorkStealingPoolTest
+ * @Description TODO
+ * @Date 2021/4/13 23:11
+ * @Created by IntelliJ IDEA
+ */
+public class WorkStealingPoolTest {
+    public static void main(String[] args) throws IOException {
+        ExecutorService service = Executors.newWorkStealingPool();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        service.execute(new R(1000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+
+        while(Thread.activeCount() > 2){
+        }
+    }
+
+    static class R implements Runnable {
+
+        int time;
+
+        R(int t) {
+            this.time = t;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(time  + " " + Thread.currentThread().getName());
+            System.out.println(Thread.activeCount());
+        }
+
+    }
+}

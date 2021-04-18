00d12132e9e50d22d89b6cbe72afc1aa2e5351c2
lemon joker <novicedriver@163.com>
2021-04-18 23:50:26 +0800

    feat: 1.disruptor

---
+++ java_02_advanced/src/main/java/indi/w4xj/disruptor/v2/LongEventFactory.java
@@ -0,0 +1,10 @@
+package indi.w4xj.disruptor.v2;
+
+import com.lmax.disruptor.EventFactory;
+
+public class LongEventFactory implements EventFactory<LongEvent> {
+
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+}
--- java_02_advanced/src/test/java/indi/w4xj/jmh/code/JmhTest.java
+++ java_02_advanced/src/test/java/indi/w4xj/jmh/code/JmhTest.java
@@ -1,6 +1,6 @@
 package indi.w4xj.jmh.code;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
 
 /**
  * @Author lemon joker
@@ -14,6 +14,21 @@ import org.openjdk.jmh.annotations.Benchmark;
 public class JmhTest {
 
     @Benchmark
+    //预热 ； iterations：次数， time：时间
+    @Warmup(iterations = 1, time = 3)
+    //用短少线程执行
+    @Fork(5)
+    /**
+     * 模式：吞吐量
+     * Throughput("thrpt", "Throughput, ops/time"),
+     * AverageTime("avgt", "Average time, time/op"),
+     * SampleTime("sample", "Sampling time"),
+     * SingleShotTime("ss", "Single shot invocation time"),
+     * All("all", "All benchmark modes");
+     */
+    @BenchmarkMode(Mode.Throughput)
+    //总共执行多少次测试
+    @Measurement(iterations = 1, time = 3)
     public void testForEach() {
         JmhBizClass.foreach();
     }

13f5751c9ecc7cf72a5914879f567c27185c5f13
lemon joker <novicedriver@163.com>
2021-05-16 23:25:40 +0800

    feat: 1、频繁FullGC  排查问题

---
+++ java_02_advanced/src/main/java/indi/w4xj/jvm/code/FullGcTest01.java
@@ -0,0 +1,66 @@
+package indi.w4xj.jvm.code;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author lemon joker
+ * @Project java_upstream
+ * @Package indi.w4xj.jvm.code
+ * @Classname FullGcTest01
+ * @Description 频繁FullGC  排查问题
+ * @Date 2021/5/16 12:52
+ * @Created by IntelliJ IDEA
+ */
+public class FullGcTest01 {
+
+    private static class CardInfo {
+        BigDecimal price = new BigDecimal("0.0");
+        String name = "张三";
+        int age = 5;
+        Date birthdate = new Date();
+        public void m() {
+
+        }
+    }
+
+    private static ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(50,
+            new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    public static void main(String[] args) throws Exception {
+        executor.setMaximumPoolSize(50);
+
+        for (;;){
+            modelFit();
+            Thread.sleep(100);
+        }
+    }
+
+    private static void modelFit(){
+        List<CardInfo> taskList = getAllCardInfo();
+        taskList.forEach(info -> {
+            // do something
+            executor.scheduleWithFixedDelay(() -> {
+                //do sth with info
+                info.m();
+
+            }, 2, 3, TimeUnit.SECONDS);
+        });
+    }
+
+    private static List<CardInfo> getAllCardInfo(){
+        List<CardInfo> taskList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            CardInfo ci = new CardInfo();
+            taskList.add(ci);
+        }
+
+        return taskList;
+    }
+}

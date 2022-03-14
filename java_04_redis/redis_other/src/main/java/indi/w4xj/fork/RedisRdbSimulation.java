9998a7f20c6e0162e9691a7902d80deedc5a7bd3
张雨涛 <novicedriver@163.com>
2022-03-14 20:34:59 +0800

    [modified]redis 1.模拟redis rdb 持久化过程

--- java_04_redis/redis_other/src/main/java/indi/w4xj/fork/RedisRdbSimulation.java
+++ java_04_redis/redis_other/src/main/java/indi/w4xj/fork/RedisRdbSimulation.java
@@ -1,17 +1,18 @@
 package indi.w4xj.fork;
 
-import javafx.scene.shape.VLineTo;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.*;
 
 
 /**
- * @Author lemon joker
+ * @Author yolo cheung
  * @Project java_upstream
  * @Package indi.w4xj.fork
  * @Classname RedisRdbSimulation
- * @Description TODO
+ * @Description 模拟redis rdb持久化
  * @Date 2022/3/8 23:17
  * @Created by IntelliJ IDEA
  */
@@ -22,8 +23,8 @@ public class RedisRdbSimulation {
     static {
         //原始redis数据
         set("a", "abc");
-        lpush("b", Arrays.asList(1,2,3));
-        hset("c", new HashMap<String, Object>(){{
+        lPush("b", Arrays.asList(1,2,3));
+        hSet("c", new HashMap<String, Object>(){{
             put("name", "zhangsan");
             put("age", 18);
             put("sex", "male");
@@ -34,15 +35,22 @@ public class RedisRdbSimulation {
     @NoArgsConstructor
     @AllArgsConstructor
     private static class Value {
+        //引用数量
         private int ref;
-        private Class<?> T ;
+        //数据类型
+        private Class<?> dataStructure ;
+        //值
         private Object value;
     }
 
     public static void main(String[] args) {
+        //副本，模拟子线程的引用
         HashMap<String, Value>  copy = new HashMap<>();
-        fork(redisData, copy);
+        //fork子进程，数据指向同一个内存区域
+        fork(copy);
+        //持久化
         persist(copy);
+        //并发修改
         parallelModify();
 
         //2即代表gc线程和main线程
@@ -50,6 +58,7 @@ public class RedisRdbSimulation {
             //让出线程执行权
             Thread.yield();
         }
+
         System.out.println(redisData.get("a"));
         System.out.println(redisData.get("b"));
         System.out.println(redisData.get("c"));
@@ -67,8 +76,8 @@ public class RedisRdbSimulation {
                 try {
                     Thread.sleep(100L);
                     set("a", "abc"+ finalI1);
-                    lpush("b", Arrays.asList(finalI1));
-                    hset("c", new HashMap<String, Object>(){{
+                    lPush("b", Arrays.asList(finalI1));
+                    hSet("c", new HashMap<String, Object>(){{
                         put("i"+ finalI1, finalI1);
                     }});
                 } catch (InterruptedException e) {
@@ -84,33 +93,36 @@ public class RedisRdbSimulation {
      */
     private static void persist(HashMap<String, Value> copy) {
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + "：持久化开始---------------" + new Date());
+            System.out.println(Thread.currentThread().getName() + "：持久化开始---" + new Date());
             Iterator<Map.Entry<String, Value>> iterator = copy.entrySet().iterator();
             while (iterator.hasNext()){
                 try {
                     Map.Entry<String, Value> item = iterator.next();
-                    System.out.println("子进程" + new Date() + "写入---\nkey=" + item.getKey() + "\nvalue=" + item.getValue() + "\n");
+                    System.out.println(Thread.currentThread().getName() +"：写入---" + new Date() +"\nkey=" + item.getKey() + "\nvalue=" + item.getValue() + "\n");
                     Thread.sleep(1000L);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             copy.clear();
-            System.out.println(Thread.currentThread().getName() + "：持久化完成---------------" + new Date());
+            System.out.println(Thread.currentThread().getName() + "：持久化完成---" + new Date()+"\n");
         }, "persistThread").start();
     }
 
 
-
-    //数据存进redis
+    /**
+     * 数据存进redis
+     * @param key
+     * @param value
+     * @param T
+     */
     private static void put(String key, Object value, Class<?> T) {
         Value original = redisData.get(key);
         //如果引用为2，则触发写时复制
         if(null != original && original.getRef() == 2){
-            System.out.println(key);
             copyOnWrite(key, value, T);
         }else {
-            redisData.put(key, new Value(1, String.class, value));
+            redisData.put(key, new Value(1, T, value));
         }
 
     }
@@ -125,7 +137,7 @@ public class RedisRdbSimulation {
         Value original = redisData.get(key);
         if(original.getRef() == 2){
             //触发写时复制
-            System.out.println(Thread.currentThread().getName() + "触发写时复制\n key[" + key + "]的值从"+ original.getValue() +"改为" + value + "\n");
+            System.out.println(Thread.currentThread().getName() + "：触发写时复制---" + new Date() + "\nkey[" + key + "]的值从"+ original.getValue() +"改为" + value + "\n");
             redisData.put(key, new Value(1, T, value));
             original.setRef(1);
         }else{
@@ -147,13 +159,30 @@ public class RedisRdbSimulation {
     }
 
     /**
+     *
+     * @param key
+     * @return
+     */
+    private static String get(String key) {
+        Value value = redisData.get(key);
+        if(value.getValue() instanceof String){
+
+        } else {
+
+        }
+        return null;
+    }
+
+
+
+    /**
      * 模拟list插入
      * @param key
      * @param value
      */
-    private static void lpush(String key, List value) {
+    private static void lPush(String key, List value) {
         if (value instanceof List){
-            redisData.put(key, new Value(1, List.class, value));
+            put(key, value, List.class);
         } else{
             throw new RuntimeException("只能放入List类型");
         }
@@ -164,9 +193,9 @@ public class RedisRdbSimulation {
      * @param key
      * @param value
      */
-    private static void hset(String key, Map value) {
+    private static void hSet(String key, Map value) {
         if (value instanceof Map){
-            redisData.put(key, new Value(1, Map.class, value));
+            put(key, value, Map.class);
         } else{
             throw new RuntimeException("只能放入Map类型");
         }
@@ -176,10 +205,9 @@ public class RedisRdbSimulation {
     /**
      * redis：fork出一个子进程，子进程中的数据是和父进程相同
      * 模拟：将原Map中的数据复制到另一个Map中，并将引用数改为2
-     * @param redisData
      * @param copy
      */
-    private static void fork(HashMap<String, Value> redisData, HashMap<String, Value> copy) {
+    private static void fork(HashMap<String, Value> copy) {
         Iterator<Map.Entry<String, Value>> iterator = redisData.entrySet().iterator();
         while(iterator.hasNext()){
             Map.Entry<String, Value> item = iterator.next();

f6ce3fce03cc23d053249aab398a64bf086ba3b3
张雨涛 <novicedriver@163.com>
2022-05-15 15:34:56 +0800

    [modified] 1.spring security主启动类

---
+++ java_08_springsecurity/spring_security_meditation_phase/src/main/java/com/w4xj/spring_security_meditation_phase/SpringSecurityMeditationPhaseApplication.java
@@ -0,0 +1,16 @@
+package com.w4xj.spring_security_meditation_phase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yolo Cheung
+ */
+@SpringBootApplication
+public class SpringSecurityMeditationPhaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMeditationPhaseApplication.class, args);
+    }
+
+}
